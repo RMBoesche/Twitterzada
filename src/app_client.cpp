c9e4690cc8f1950b6fffb4367390dec97e182740
Lucas1603 <lucasbalbinot03@gmail.com>
2022-02-23 21:02:31 -0300

    string parse client

--- src/app_client.cpp
+++ src/app_client.cpp
@@ -4,56 +4,102 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <time.h>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-#define PORT 4000
+typedef struct __packet{
+	uint16_t type; //Tipo do pacote (p.ex. DATA | CMD)
+	uint16_t seqn; //Número de sequência
+	uint16_t length; //Comprimento do payload
+	uint16_t timestamp; // Timestamp do dado
+	const char* _payload; //Dados da mensagem
+ } packet;
+
 
 int main(int argc, char *argv[])
 {
     int sockfd, n;
-    unsigned int length;
-    struct sockaddr_in serv_addr, from;
-    struct hostent *server;
+	unsigned int length;
+	struct sockaddr_in serv_addr, from;
+	struct hostent *server;
+	int PORT, timestamp;
+	std::string profileName;
+	std::string all_data, command;
 
-    char buffer[256];
-    if (argc < 2)
-    {
-        fprintf(stderr, "usage %s hostname\n", argv[0]);
+	char buffer[256];
+	if (argc < 2) {
+		fprintf(stderr, "usage %s hostname\n", argv[0]);
+		exit(0);
+	}
+	
+	// get profile name
+	profileName = argv[1];
+
+	// get hostname
+	server = gethostbyname(argv[2]);
+	if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
         exit(0);
-    }
+    }	
 
-    server = gethostbyname(argv[1]);
-    if (server == NULL)
-    {
-        fprintf(stderr, "ERROR, no such host\n");
-        exit(0);
-    }
+	// get PORT
+	PORT = atoi(argv[3]);
 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-        printf("ERROR opening socket");
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+		printf("ERROR opening socket");
+	
+	serv_addr.sin_family = AF_INET;     
+	serv_addr.sin_port = htons(PORT);    
+	serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
+	bzero(&(serv_addr.sin_zero), 8);
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
-    bzero(&(serv_addr.sin_zero), 8);
+	printf("Enter the message: ");
+	bzero(buffer, 256);
 
-    printf("Enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 256, stdin);
+	// get the 'SEND' or 'FOLLOW'
+	std::cin >> command;
+	
+	if( command == std::string("SEND") ) {
+		std::cout << "SEND";
 
-    n = sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *)&serv_addr, sizeof(struct sockaddr_in));
-    if (n < 0)
-        printf("ERROR sendto");
+		// get the message to be sent
+		std::getline(std::cin, all_data);
+		strcpy(buffer, all_data.c_str());
+	}
+	else if( command == std::string("FOLLOW") ) {
+		std::cout << "FOLLOW";
+		// get the user to be followed
+		std::getline(std::cin, all_data);	
 
-    length = sizeof(struct sockaddr_in);
-    n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *)&from, &length);
-    if (n < 0)
-        printf("ERROR recvfrom");
+	}
+	else {
+		std::cout << "PLEASE ENTER ONE OF THE FOLLOWING COMMANDS:\n\tSEND\n\tFOLLOW" << std::endl;
+	}
 
-    printf("Got an ack: %s\n", buffer);
+	std::cout << all_data << std::endl;
+	std::string message = all_data.substr( all_data.find(' ') );
+	// fgets(buffer, 256, stdin);
 
-    close(sockfd);
-    return 0;
+	std::cout << buffer << std::endl;
+	timestamp = (int)time(NULL);
+	std::cout << timestamp << std::endl;
+
+	n = sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
+	if (n < 0) 
+		printf("ERROR sendto");
+	
+	length = sizeof(struct sockaddr_in);
+	n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *) &from, &length);
+	if (n < 0)
+		printf("ERROR recvfrom");
+
+	printf("Got an ack: %s\n", buffer);
+	
+	close(sockfd);
+	return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,13 +1,36 @@
 #include <sys/types.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <time.h>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
 
 #define PORT 4000
+
+typedef struct __packet{
+	uint16_t type; //Tipo do pacote (p.ex. DATA | CMD)
+	uint16_t seqn; //Número de sequência
+	uint16_t length; //Comprimento do payload
+	uint16_t timestamp; // Timestamp do dado
+	const char* _payload; //Dados da mensagem
+} packet;
+
+typedef struct __notification{
+	uint32_t id; //Identificador da notificação (sugere-se um identificador único)
+	uint32_t timestamp; //Timestamp da notificação
+	uint16_t length; //Tamanho da mensagem
+	uint16_t pending; //Quantidade de leitores pendentes
+	const char* _string; //Mensagem
+} notification;
+
 
 int main(int argc, char *argv[])
 {
@@ -15,34 +38,35 @@ int main(int argc, char *argv[])
 	socklen_t clilen;
 	struct sockaddr_in serv_addr, cli_addr;
 	char buf[256];
-
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+		
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) 
 		printf("ERROR opening socket");
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(serv_addr.sin_zero), 8);
-
-	if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr)) < 0)
+	bzero(&(serv_addr.sin_zero), 8);    
+	 
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(struct sockaddr)) < 0) 
 		printf("ERROR on binding");
-
+	
 	clilen = sizeof(struct sockaddr_in);
-
-	while (1)
-	{
+	
+	int b = 100;
+	while (b--) {
 		/* receive from socket */
-		n = recvfrom(sockfd, buf, 256, 0, (struct sockaddr *)&cli_addr, &clilen);
-		if (n < 0)
+		n = recvfrom(sockfd, buf, 256, 0, (struct sockaddr *) &cli_addr, &clilen);
+
+		if (n < 0) 
 			printf("ERROR on recvfrom");
 		printf("Received a datagram: %s\n", buf);
-
+		
 		/* send to socket */
-		n = sendto(sockfd, "Got your message\n", 17, 0, (struct sockaddr *)&cli_addr, sizeof(struct sockaddr));
-		if (n < 0)
+		n = sendto(sockfd, "Got your message\n", 17, 0,(struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
+		if (n  < 0) 
 			printf("ERROR on sendto");
 	}
-
+	
 	close(sockfd);
 	return 0;
 }
